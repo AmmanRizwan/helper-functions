6084a7dcb0a805e8a12cf9399174668bb0d0babd
Amman Rizwan <97797756+AmmanRizwan@users.noreply.github.com>
2025-10-07 20:38:44 +0530

    add: enhance README with detailed ML prediction steps; implement functions for handling missing values and visualizing clusters in helper scripts

--- helper-2.py
+++ helper-2.py
@@ -17,3 +17,49 @@ def visualize_pair_plot(df):
 def visualize_dimesion_reduction(df):
   pass
 
+# Clusters Ploting
+
+def visualize_clusters():
+  plt.scatter(df["Feature 1"], df['Feature 2'], c=df['Cluster'], cmap='viridis', s=50)
+  plt.xlabel("Feature 1")
+  plt.ylabel("Feature 2")
+  plt.title("Clusters Found by K-Means")
+  plt.show()
+
+# KMeans prediction
+
+def kmean_prediction():
+  score = silhouette_score(df[['Feature', "Features..."]], df['Cluster'])
+  print("Score:", score)
+  
+# DBSCAN prediction
+
+def dbscan_prediction():
+  mask = cluster_labels != -1
+  score = silhouette_score(X[mask], cluster_labels[mask])
+  print("Score", score)
+  
+def remove_html(text):
+  pattern = re.compile('<.*?>')
+  return pattern.sub('', text)
+
+def remove_url(text):
+  pattern = re.compile(r'https?://\S+|www\.\S+')
+  return pattern.sub("", text)
+
+def remove_mentions(text):
+  pattern = re.compile("@\S+")
+  return pattern.sub("", text)
+
+def remove_digits(text):
+  pattern = re.compile(r'\d+')
+  return pattern.sub("", text)
+
+def remove_symbols(text):
+  return re.sub(r'[^\x00-\x7F]+', "", str(text))
+
+
+def remove_punc(text):
+  punc = string.punctuation
+  text_nonPunc = ''.join([char for char in text if char not in punc])
+  return text_nonPunc
